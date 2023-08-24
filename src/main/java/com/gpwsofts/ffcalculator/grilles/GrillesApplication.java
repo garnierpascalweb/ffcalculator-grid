f2bf67cd8976271e7a1342ff953d11d0fefa3a66
Garnier <pascalscn@hotmail.com>
2023-08-24 13:51:26 +0200

    1.0.0: generate project

---
+++ src/main/java/com/gpwsofts/ffcalculator/grilles/GrillesApplication.java
@@ -0,0 +1,13 @@
+package com.gpwsofts.ffcalculator.grilles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrillesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrillesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpwsofts/ffcalculator/grilles/GrillesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpwsofts.ffcalculator.grilles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrillesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
