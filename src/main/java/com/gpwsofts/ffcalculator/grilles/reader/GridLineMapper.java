9c1bf6b61114cb7db64b7fb78d6606bab34df197
Garnier <pascalscn@hotmail.com>
2023-08-24 14:44:41 +0200

    0.0.1: mise en place du line mapper

---
+++ src/main/java/com/gpwsofts/ffcalculator/grilles/reader/GridLineMapper.java
@@ -0,0 +1,14 @@
+package com.gpwsofts.ffcalculator.grilles.reader;
+
+import org.springframework.batch.item.file.LineMapper;
+
+import com.gpwsofts.ffcalculator.grilles.model.InputGrid;
+
+public class GridLineMapper implements LineMapper<InputGrid> {
+
+	@Override
+	public InputGrid mapLine(String line, int number) throws Exception {
+		
+	}
+
+}
